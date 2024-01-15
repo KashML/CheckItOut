941e80d09e098a57703b2df37ff060a5db476179
KashML <abhinavkashyap.us@gmail.com>
2024-01-14 20:35:08 -0800

    test 6

--- main/icons/__init__.py
+++ main/icons/__init__.py
@@ -4,4 +4,4 @@ from pathlib import Path
 CURRENT_FILE_PATH = os.path.realpath(__file__)
 ICON_DIR = Path(CURRENT_FILE_PATH).parent
 
-SAVE_ICON = os.path.join(ICON_DIR, "icon_1.svg")
+SAVE_ICON = os.path.join(ICON_DIR, "icon_2.svg")
